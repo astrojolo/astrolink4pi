a2289b7c24f43d39bd383567431f34bce4219e2b
astrojolo <astrojolo@astrojolo.com>
2021-11-18 13:01:13 +0100

    SPI test

--- astrolink4pi.h
+++ astrolink4pi.h
@@ -150,7 +150,7 @@ private:
 	void stepperStandby(bool state);
 	void systemUpdate();
 	void pwmCycle();
-	int setDac(int value);
+	int setDac(int chan, int value);
 	long int millis();
 
 	static constexpr const char *SYSTEM_TAB {"System"};
