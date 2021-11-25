bb0df745acb4a3fddefce6d66a73a8396a687074
astrojolo <astrojolo@astrojolo.com>
2021-11-25 14:14:05 +0100

    Focuser

--- indi_astrolink4pi.xml.cmake
+++ indi_astrolink4pi.xml.cmake
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
 <devGroup group="Auxiliary">
-	<device label="AstroLink 4 Pi">
+	<device label="AstroLink 4 Pi" manufacturer="Astrojolo">
 		<driver name="AstroLink 4 Pi">indi_astrolink4pi</driver>
 		<version>@VERSION_MAJOR@.@VERSION_MINOR@</version>
 	</device>
 </devGroup>
 <devGroup group="Focusers">
-	<device label="AstroLink 4 Pi Focuser">
+	<device label="AstroLink 4 Pi Focuser" manufacturer="Astrojolo">
 		<driver name="AstroLink 4 Pi Focuser">indi_astrolink4pi</driver>
 		<version>@VERSION_MAJOR@.@VERSION_MINOR@</version>
 	</device>
