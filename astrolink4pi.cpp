a26b984f7bc321caffdf79a992f77dad37be28e3
astrojolo <astrojolo@astrojolo.com>
2022-07-14 16:59:09 +0200

    SHT support

--- astrolink4pi.cpp
+++ astrolink4pi.cpp
@@ -1608,7 +1608,7 @@ bool AstroLink4Pi::readSHT()
 
 		double a = 17.271;
 		double b = 237.7;
-		double temp = (a * celsius) / (b + celsius) + log(humidity * 0.01);
+		double temp = (a * cTemp) / (b + cTemp) + log(humidity * 0.01);
 		double Td = (b * temp) / (a - temp);
 
 		setParameterValue("WEATHER_TEMPERATURE", cTemp);
