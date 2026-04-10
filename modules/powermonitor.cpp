e2bd3529ddf994b0f7737b27a30819bd219ba671
Łukasz Socha <drjolo@gmail.com>
2026-04-10 08:48:04 +0200

    Power monitor

--- modules/powermonitor.cpp
+++ modules/powermonitor.cpp
@@ -45,7 +45,7 @@ bool PowerMonitor::isOpen() const
 
 bool PowerMonitor::read(PowerMonitor::Readings &out)
 {
-    DEBUGFDEVICE("PowerMonitor", INDI::Logger::DBG_ERROR, "TEST ERROR %d", 1);
+    DEBUGFDEVICE("PowerMonitor", INDI::Logger::DBG_SESSION, "TEST ERROR %d", 1);
     if (!isOpen())
         return false;
 
